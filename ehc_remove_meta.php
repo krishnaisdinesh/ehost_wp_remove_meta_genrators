0020fca9436b29b65c37ef24bfd8ba78a53f1534
Dinesh kumar krishna <krishna.is.dinesh@gmail.com>
2017-10-27 13:33:00 +0530

    Update ehc_remove_meta.php
    Change Hoddy greeting to welcome, remove versions from url, remove wordpress versions

--- ehc_remove_meta.php
+++ ehc_remove_meta.php
@@ -33,4 +33,36 @@ function remove_layerslider_meta_tag($html) {
 }
 add_filter( 'ls_meta_generator', 'remove_layerslider_meta_tag' );
 
+/******************************************************************/
+//Get Rid of “Howdy”
+add_filter('gettext', 'change_howdy', 10, 3);
+
+function change_howdy($translated, $text, $domain) {
+
+    if (!is_admin() || 'default' != $domain)
+        return $translated;
+
+    if (false !== strpos($translated, 'Howdy'))
+        return str_replace('Howdy', 'Welcome', $translated);
+
+    return $translated;
+}
+
+// Hide WordPress Version Info
+function hide_wordpress_version() {
+	return '';
+}
+add_filter('the_generator', 'hide_wordpress_version');
+
+// Remove WordPress Meta Generator
+remove_action('wp_head', 'wp_generator');
+
+// Remove WordPress Version Number In URL Parameters From JS/CSS
+function hide_wordpress_version_in_script($src, $handle) {
+    $src = remove_query_arg('ver', $src);
+	return $src;
+}
+add_filter( 'style_loader_src', 'hide_wordpress_version_in_script', 10, 2 );
+add_filter( 'script_loader_src', 'hide_wordpress_version_in_script', 10, 2 );
+
 ?>
